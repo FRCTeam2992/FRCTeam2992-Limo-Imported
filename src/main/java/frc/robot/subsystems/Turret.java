abc51257337f8eed8724f741fc2f46844161df98
Gerad <gerardg276@icloud.com>
2023-11-07 20:01:16 -0600

    Offset Fixes

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -113,9 +113,9 @@ public class Turret extends SubsystemBase {
             }
 
         // Update Dashboard
-        // SmartDashboard.putNumber("Turret CanCoder Real", getCanCoderRealDegrees());
-        // SmartDashboard.putNumber("Turret RobotCentric Angle", angleOverlap(getTurretAngle()));
-        // SmartDashboard.putNumber("Turret Falcon Real", getFalconRealDegrees());
+        SmartDashboard.putNumber("Turret CanCoder Real", getCanCoderRealDegrees());
+        SmartDashboard.putNumber("Turret RobotCentric Angle", angleOverlap(getTurretAngle()));
+        SmartDashboard.putNumber("Turret Falcon Real", getFalconRealDegrees());
         // SmartDashboard.putNumber("Turret Falcon Encoder Clicks", turretFalcon.getSelectedSensorPosition());
         // SmartDashboard.putNumber("Turret Target", turretTargetAngle);
         // SmartDashboard.putNumber("Camera Angle", limeLightCamera.getCameraAngle(Constants.distanceTest, Constants.cameraHeight, Constants.goalHeight));
